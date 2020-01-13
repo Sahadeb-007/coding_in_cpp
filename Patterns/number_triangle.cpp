560ee58168063d1a66910c4b53564e727a0e3792
Sahadeb-007 <soren_sahadeb@outlook.com>
2020-01-13 20:19:27 +0530

    num pattern

--- Patterns/number_triangle.cpp
+++ Patterns/number_triangle.cpp
@@ -4,13 +4,18 @@ using namespace std;
 
 int main()
 {
-    int n;
+    int n,k=1;
     cout<<"Enter Number of Rows: ";
     cin>>n;
     cout<<endl;
-    for(int i=0;i<=n;i++)
+    for(int i=1;i<=n;i++)
     {
-        cout<<i;
+        for(int j=1;j<=i;j++)
+        {
+            cout<<k<<" ";
+            k++;
+        }
+        cout<<endl;
     }
 
     return 0;
